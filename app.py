88d9500ecb15f3b228461f18f5f5c55094402783
gongju-byte <nurulizzatihayat@gmail.com>
2025-01-31 17:47:40 +0800

    added index

--- app.py
+++ app.py
@@ -2,15 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template("index.html")
-
-@app.route('/bar')
-def bar():
-    return render_template("taskbar.html")
-
-@app.route('/lock', methods=["post", "get"])
+@app.route('/', methods=["post", "get"])
 def lock():
     if request.method == 'POST':
 
@@ -23,11 +15,7 @@ def lock():
         if 'lock' in request.form:
             return redirect(url_for('lock'))
 
-    return render_template('lockscreen.html')
-
-@app.route('/test')
-def test():
-    return render_template("test.html")
+    return render_template('index.html')
 
 @app.route('/desktop')
 def desktop():
